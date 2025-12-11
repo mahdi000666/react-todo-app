0cb1d453133ff19548880b6f1ef2b5c2acea8182
Mahdi <mahdi.mlika123@gmail.com>
2025-12-11 20:37:51 +0100

    fix

---
+++ src/test-setup.js
@@ -0,0 +1,8 @@
+const store = {};
+
+global.localStorage = {
+  getItem: (key) => store[key] || null,
+  setItem: (key, value) => { store[key] = value; },
+  removeItem: (key) => { delete store[key]; },
+  clear: () => { Object.keys(store).forEach(key => delete store[key]); }
+};
